b8f6bc8e430e6bf77f6980bd56cf559102ce2ade
gogumario <ggmario@naver.com>
2017-05-24 16:43:15 +0900

    인트로 호출시 버전 체크 후 하위 버전 특정 URL 웹 브라우저 호출 처리

--- app/src/main/java/com/eluo/project/intranet/intro/IntroActivity.java
+++ app/src/main/java/com/eluo/project/intranet/intro/IntroActivity.java
@@ -2,16 +2,36 @@ package com.eluo.project.intranet.intro;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.eluo.project.intranet.MainActivity;
 import com.eluo.project.intranet.R;
 import com.eluo.project.intranet.network.NetworkUtil;
+import com.eluo.project.intranet.utils.ThreadPolicy;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /*
  * Project	    : Eluo Intranet
@@ -54,11 +74,64 @@ public class IntroActivity extends Activity {
         editor.putString("check", "exist");
         editor.commit();
 
+
         if (NetworkUtil.isNetworkConnected(this)) {
-            requestWindowFeature(Window.FEATURE_NO_TITLE); //인트로화면이므로 타이틀바를 없앤다
-            setContentView(R.layout.activity_intro);
-            h = new Handler(); //딜래이를 주기 위해 핸들러 생성
-            h.postDelayed(mrun, 2000); // 딜레이 ( 런어블 객체는 mrun, 시간 2초)
+            String sVersionName = "1.0.0";
+            try {
+                sVersionName =  getPackageManager().getPackageInfo(getPackageName(),0).versionName;
+                new ThreadPolicy();
+                String result = SendByHttp(""); // 메시지를 서버에 보냄
+                String sVer = "";
+                if (result != null) {
+                    String[][] parsedData = jsonParserList(result);
+                    if(parsedData != null){
+                        sVer = parsedData[0][0];
+                    }else{
+                        sVer = sVersionName;
+                        Log.i("버전 체크:","앱 버전 체크 실패 하였습니다");
+                        //FirebaseCrash.report(new Exception("앱 버전 체크 실패 하였습니다"));
+                    }
+                }
+
+                if(!sVersionName.equals(sVer)){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    // 여기서 부터는 알림창의 속성 설정
+                    builder.setTitle(R.string.D_TitleName)
+                            .setMessage(R.string.D_Update)
+                            .setCancelable(false)
+                            .setPositiveButton(R.string.D_Approval, new DialogInterface.OnClickListener(){
+                                // 확인 버튼 클릭시 설정
+                                public void onClick(DialogInterface dialog, int whichButton){
+                                    finish();
+                                    callBrowser("http://fs.eluocnc.com:8282/html/adn_download.html");
+                                }
+                            })
+                            .setNegativeButton(R.string.D_Canceled, new DialogInterface.OnClickListener(){
+                                // 취소 버튼 클릭시 설정
+                                public void onClick(DialogInterface dialog, int whichButton){
+                                    finish();
+                                }
+                            });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }else{
+                    requestWindowFeature(Window.FEATURE_NO_TITLE); //인트로화면이므로 타이틀바를 없앤다
+                    setContentView(R.layout.activity_intro);
+                    h = new Handler(); //딜래이를 주기 위해 핸들러 생성
+                    h.postDelayed(mrun, 2000); // 딜레이 ( 런어블 객체는 mrun, 시간 2초)
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+                AlertDialog.Builder alert = new AlertDialog.Builder(IntroActivity.this);
+                alert.setPositiveButton(R.string.D_Approval, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                alert.setMessage(R.string.network_error_msg);
+                alert.show();
+            }
         }else{
             AlertDialog.Builder alert = new AlertDialog.Builder(IntroActivity.this);
             alert.setPositiveButton(R.string.D_Approval, new DialogInterface.OnClickListener() {
@@ -71,6 +144,7 @@ public class IntroActivity extends Activity {
             alert.show();
         }
     }
+
 
     Runnable mrun = new Runnable(){
         @Override
@@ -88,5 +162,97 @@ public class IntroActivity extends Activity {
     public void onBackPressed(){
         super.onBackPressed();
         h.removeCallbacks(mrun);
+    }
+
+
+    public void callBrowser(String url) {
+        try {
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(i);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private String SendByHttp(String msg) {
+
+        String URL ="http://fs.eluocnc.com:8282/versionName.jsp";
+
+        DefaultHttpClient client = new DefaultHttpClient();
+        try {
+            HttpPost post = new HttpPost(URL);
+			/* 지연시간 최대 3초 */
+            HttpParams params = client.getParams();
+            HttpConnectionParams.setConnectionTimeout(params, 3000);
+            HttpConnectionParams.setSoTimeout(params, 300);
+
+			/* 데이터 보낸 뒤 서버에서 데이터를 받아오는 과정 */
+            HttpResponse response = null;
+            try{
+                ConnectivityManager conManager =(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo netInfo = conManager.getActiveNetworkInfo();
+
+                if(netInfo != null && netInfo.isConnected()){
+                    response = client.execute(post);
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            BufferedReader bufreader = new BufferedReader( new InputStreamReader(response.getEntity().getContent(),"utf-8"));
+            String line = null;
+            String result = "";
+            while ((line = bufreader.readLine()) != null) {
+                result += line;
+            }
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            client.getConnectionManager().shutdown();	// 연결 지연 종료
+            return "";
+        }
+    }
+    /**
+     * 받은 JSON 객체를 파싱하는 메소드(회의실 리스트)
+     * @param
+     * @return
+     */
+    private String[][] jsonParserList(String pRecvServerPage) {
+        Log.i("서버에서 받은 전체 내용 : ", pRecvServerPage);
+        try {
+            JSONObject json = new JSONObject(pRecvServerPage);
+            JSONArray jArr = json.getJSONArray("vers");
+            String jsonName[];
+            if(pRecvServerPage.lastIndexOf("RESULT") > 0) {
+                String[] jsonName1 = {"RESULT"};
+                jsonName = jsonName1;
+            }else{
+                String[] jsonName1 = {"VER"};
+                jsonName = jsonName1;
+            }
+
+            String[][] parseredData = new String[jArr.length()][jsonName.length];
+            if(parseredData.length > 0){
+                for (int i = 0; i < jArr.length(); i++) {
+                    json = jArr.getJSONObject(i);
+                    for(int j = 0; j < jsonName.length; j++) {
+                        try{
+                            if(parseredData[i][j] == null ){
+                                parseredData[i][j] = json.getString(jsonName[j]);
+                                Log.i("JSON을 분석한 데이터 " + i + " : ", parseredData[i][j] );
+                            }
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }else{
+                Toast.makeText(getApplicationContext(), R.string.network_error_retry, Toast.LENGTH_SHORT).show();
+            }
+            return parseredData;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
