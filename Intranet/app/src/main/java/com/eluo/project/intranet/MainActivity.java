d36539a2e650c7b85ecd29d8b9b0e6061da8f690
gogumario <ggmario@naver.com>
2017-05-26 15:20:19 +0900

    소스 정리

--- Intranet/app/src/main/java/com/eluo/project/intranet/MainActivity.java
+++ Intranet/app/src/main/java/com/eluo/project/intranet/MainActivity.java
@@ -65,7 +65,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.UUID;
 
 /*
  * Project	    : Eluo Intranet
